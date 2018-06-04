1ec732f3ce8d5db3794a0d996dafadc2d4d161f0
fcernafukuzaki <fcernaf@gmail.com>
2018-06-03 23:02:13 -0500

    Redux

--- src/main/resources/static/entries/home.js
+++ src/main/resources/static/entries/home.js
@@ -1,13 +1,30 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import Home from '../pages/containers/Home';
-// import Playlist from './src/playlist/components/playlist';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
+import { persistStore, autoRehydrate } from 'redux-persist';
+
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import reducers from '../app/reducers';
+
+const store = createStore(
+	reducers,
+	//initialState,
+	applyMiddleware(
+		thunk,
+		logger
+	),
+	autoRehydrate()
+);
 
 const homeContainer = document.getElementById('home-container');
 
-//ReactDOM.
-render( <Home />, homeContainer);
-
-
+render(
+  <Provider store={store}>
+    <Home />
+  </Provider>
+  , homeContainer);
