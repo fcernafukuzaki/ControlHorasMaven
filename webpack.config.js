7364b8ff2e60db284e453d34847d295ce93a33c2
fcernafukuzaki <fcernaf@gmail.com>
2018-06-03 23:01:17 -0500

    Redux

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-    entry: './src/main/resources/static/entries/home.js',
-    devtool: 'sourcemaps',
-    cache: true,
-    debug: true,
-    output: {
-        path: __dirname,
-        filename: './src/main/resources/static/home.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
-    }
+  entry: './src/main/resources/static/entries/home.js',
+  devtool: 'sourcemaps',
+  cache: true,
+  debug: true,
+  output: {
+    path: __dirname,
+    filename: './src/main/resources/static/home.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: path.join(__dirname, '.'),
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
 };
