b0276a86ad607936634a44d46605d9bec01169ba
fcernafukuzaki <fcernaf@gmail.com>
2018-06-24 00:43:30 -0500

    Prueba

--- src/main/resources/static/home.js
+++ src/main/resources/static/home.js
@@ -18636,8 +18636,6 @@
 	
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 	
-	//<img src="favicon.ico" width="30" height="30" className="d-inline-block align-top" />
-	
 	var NavBar = function (_Component) {
 		_inherits(NavBar, _Component);
 	
@@ -18656,6 +18654,7 @@
 					_react2.default.createElement(
 						"a",
 						{ className: "navbar-brand", href: "#" },
+						_react2.default.createElement("img", { src: "favicon.ico", width: "30", height: "30", className: "d-inline-block align-top" }),
 						"Control Horas"
 					),
 					_react2.default.createElement(
