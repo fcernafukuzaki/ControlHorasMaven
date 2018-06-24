45f011b8bb8312f3abffa9214d8545d88d33cade
fcernafukuzaki <fcernaf@gmail.com>
2018-06-24 00:23:30 -0500

    Prueba base de datos

--- src/main/java/controlhoras/security/oauth/CustomUserDetailsService.java
+++ src/main/java/controlhoras/security/oauth/CustomUserDetailsService.java
@@ -9,16 +9,16 @@ import org.springframework.transaction.annotation.Transactional;
 
 import controlhoras.security.bean.Usuario;
 import controlhoras.security.component.Roles;
-//import controlhoras.security.repository.UsuarioService;
+import controlhoras.security.repository.UsuarioService;
 
 @Service
 public class CustomUserDetailsService extends Usuario implements UserDetailsService {
 
 	private static final long serialVersionUID = 1L;
-/*
+
 	@Autowired
 	private UsuarioService usuarioService;
-	*/
+	
 	CustomUserDetails customUserDetails;
 	
 	@Autowired
@@ -27,16 +27,12 @@ public class CustomUserDetailsService extends Usuario implements UserDetailsServ
 	@Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String email) {
-    	/*Usuario usuario = usuarioService.findByNombreUsuario(email);
+    	Usuario usuario = usuarioService.findByNombreUsuario(email);
     	
     	if (usuario == null) {
             throw new UsernameNotFoundException("No se encontró usuario con nombre: " + email);
         }
-    	*/
-		Usuario usuario = new Usuario();
-		usuario.setIdUsuario(1);
-		usuario.setNombreUsuario("fcernaf@gmail.com");
-		
+    	
     	this.customUserDetails = new CustomUserDetails(usuario);
     	return this.customUserDetails;
     }
@@ -45,20 +41,17 @@ public class CustomUserDetailsService extends Usuario implements UserDetailsServ
      * Metodos para autenticar usuario
      */
     public boolean isAdministrador() {
-    	/*Usuario usuario = usuarioService.findByNombreUsuarioAndPerfilesNombrePerfil(customUserDetails.getNombreUsuario(), roles.ROLE_ADMINISTRADOR);
-    	return (usuario == null ? false : true);*/
-    	return true;
+    	Usuario usuario = usuarioService.findByNombreUsuarioAndPerfilesNombrePerfil(customUserDetails.getNombreUsuario(), roles.ROLE_ADMINISTRADOR);
+    	return (usuario == null ? false : true);
     }
     
     public boolean isSupervisor() {
-    	/*Usuario usuario = usuarioService.findByNombreUsuarioAndPerfilesNombrePerfil(customUserDetails.getNombreUsuario(), roles.ROLE_SUPERVISOR);
-    	return (usuario == null ? false : true);*/
-    	return true;
+    	Usuario usuario = usuarioService.findByNombreUsuarioAndPerfilesNombrePerfil(customUserDetails.getNombreUsuario(), roles.ROLE_SUPERVISOR);
+    	return (usuario == null ? false : true);
     }
     
     public boolean isAnalistaFabrica() {
-    	/*Usuario usuario = usuarioService.findByNombreUsuarioAndPerfilesNombrePerfil(customUserDetails.getNombreUsuario(), roles.ROLE_ANALISTA_FABRICA);
-    	return (usuario == null ? false : true);*/
-    	return true;
+    	Usuario usuario = usuarioService.findByNombreUsuarioAndPerfilesNombrePerfil(customUserDetails.getNombreUsuario(), roles.ROLE_ANALISTA_FABRICA);
+    	return (usuario == null ? false : true);
     }
 }
